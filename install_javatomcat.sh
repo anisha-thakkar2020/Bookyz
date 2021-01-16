1b239cd9370625ccf308aaf80300f5617b620ea3
anisha-thakkar2020 <71973583+anisha-thakkar2020@users.noreply.github.com>
2021-01-15 18:13:28 -0600

    Update install_javatomcat.sh
    Install script for Java and Tomcat, also grants access to tomcat user

--- install_javatomcat.sh
+++ install_javatomcat.sh
@@ -1,5 +1,8 @@
+## install jdk##
 sudo apt-get update
 sudo apt-get install default-jdk
+
+## add tomcat user to group and grant permission, install tomcat##
 sudo groupadd tomcat
 sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
 cd /tmp
